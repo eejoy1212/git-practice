43332a90e3a3197b78ed4dc6ca1003bfeaffd610
eejoy1212 <79134254+eejoy1212@users.noreply.github.com>
2021-05-19 18:16:20 +0900

    Add

---
+++ Alssong/src/com/tj/lwh/Chap02.java
@@ -0,0 +1,9 @@
+package com.tj.lwh;
+
+public class Chap02 {
+public static void main(String[] args) {
+	System.out.println("82+17="+(82+17));
+	System.out.println("82-17="+(82-17));
+	
+}
+}
---
+++ Alssong/src/com/tj/lwh/Hello.java
@@ -0,0 +1,7 @@
+package com.tj.lwh;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("이\n\n원\n희");
+}
+}
